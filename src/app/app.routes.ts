5c9ac218ba76851e8637adb05f3be841e5ff84e2
Devcor28 <horaciocornejo28@gmail.com>
2024-10-31 20:16:34 -0300

    mi 1er commit portafolio angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { ProjectsComponent } from './paginas/projects/projects.component';
+import { ContactComponent } from './paginas/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', redirectTo: '', pathMatch:'full'}
+];
